34b3ae59cbfa177b028c1d12cae9221c9e772b30
Big-Mike-Edahow <perigran_falcon@yahoo.com>
2026-02-25 21:56:46 -0700

    Initial commit.

---
+++ Hello_World/static/js/script.js
@@ -0,0 +1,40 @@
+// script.js
+
+class Example extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("static/");
+    this.load.image("sky", "images/space3.png");
+    this.load.image("logo", "images/phaser3-logo.png");
+    this.load.image("red", "images/red.png");
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+    const particles = this.add.particles(0, 0, "red", {
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: "ADD"
+    });
+
+    const logo = this.physics.add.image(400, 100, "logo");
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+    particles.startFollow(logo);
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: Example,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
---
+++ README.md
@@ -0,0 +1,2 @@
+# PhaserJS Examples
+My versions of various examples on the PhaserJS website.
